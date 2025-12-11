8f62bcaac62787db1fec8c940d85d1bdd0a6807e
kaseradeepak <deepak.kasera@scaler.com>
2025-12-11 22:01:18 +0530

    11th Dec code

--- Kanban board project/script.js
+++ Kanban board project/script.js
@@ -3,11 +3,15 @@ let addBtn = document.querySelector('.add-btn')
 let modalCont = document.querySelector('.modal-cont')
 let mainCont = document.querySelector('.main-cont')
 let taskArea = document.querySelector('.textArea-cont')
+let allPriorityColors = document.querySelectorAll('.priority-color')
 
 let isModalVisible = false 
 
 const lockClass = 'fa-lock'
 const unlockClass = 'fa-lock-open'
+
+let ticketColor = 'lightpink'
+let colors = ['lightpink', 'lightgreen', 'lightblue', 'black']
 
 //If user is clicking on add-btn, then the modal should pop-up
 addBtn.addEventListener('click', function() {
@@ -37,7 +41,7 @@ modalCont.addEventListener('keydown', function(event) {
         let taskText = taskArea.value 
 
         ticketCont.innerHTML = `
-            <div class="ticket-color" style="background-color: lightpink"></div>
+            <div class="ticket-color" style="background-color: ${ticketColor}"></div>
             <div class="ticket-id">${ticketId}</div>
             <div class="task-area">${taskText}</div>
             <div class="ticket-lock">
@@ -55,6 +59,7 @@ modalCont.addEventListener('keydown', function(event) {
 
         //Handle lock for this ticket.
         handleLock(ticketCont)
+        handleTicketColor(ticketCont)
     }
 })
 
@@ -82,3 +87,41 @@ function handleLock(ticket) {
     })
 }
 
+
+// Moving the active class to the color which has been clicked by the user.
+
+allPriorityColors.forEach(function(colorElem) {
+    colorElem.addEventListener('click', function() {
+        allPriorityColors.forEach(function(priorityColor) {
+            priorityColor.classList.remove('active')
+        } ) 
+
+        colorElem.classList.add('active')
+        ticketColor = colorElem.classList[0]
+    })
+})
+
+
+//Change the color of ticket band on user click.
+function handleTicketColor(ticket) {
+    let ticketColorBand = ticket.querySelector('.ticket-color')
+
+    ticketColorBand.addEventListener('click', function() {
+        let currentColor = ticketColorBand.style.backgroundColor;
+
+
+        //Find the index of current color in the list of colors.
+        let currentColorIndex
+        for(let i = 0; i < 4; i++) {
+            if(colors[i] === currentColor) {
+                currentColorIndex = i
+            }
+        }
+
+        currentColorIndex++
+
+        //TODO: Implement modulo logic.
+
+        console.log(currentColorIndex)
+    })
+}
